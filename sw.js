be7afb9539cdeb96a3c48a4f41876e40d3b9a05b
sumteck <tksumesh85@gmail.com>
2026-04-06 18:29:50 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,26 @@
-self.addEventListener('fetch', (event) => {
-  // ഓഫ്‌ലൈൻ വർക്കിംഗിന് വേണ്ടിയുള്ള ബേസിക് കോഡ്
+const cacheName = 'office-proceedings-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './logo192.png',
+  './logo512.png'
+];
+
+// ഇൻസ്റ്റാൾ ചെയ്യുമ്പോൾ ഫയലുകൾ സേവ് ചെയ്യുന്നു
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+// ആപ്പ് പ്രവർത്തിപ്പിക്കുമ്പോൾ സേവ് ചെയ്ത ഫയലുകൾ കാണിക്കുന്നു
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
